b29de914406ada9dc3fbbf2bb2e36903819bab76
Muhammad Anas <anas31.5.2002@gmail.com>
2026-06-05 15:07:43 +0500

    update to deploy BE on vercel

--- server.js
+++ server.js
@@ -31,6 +31,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/wishlist", wishlistRoutes);
 app.use("/api/order", Orderroutes);
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`),
-);
+export default app;
