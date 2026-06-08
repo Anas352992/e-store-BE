c238f2bd615e962e00bd3e636f20f6e21c9f042c
Muhammad Anas <anas31.5.2002@gmail.com>
2026-06-08 18:11:10 +0500

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "https://e-store-fe.vercel.app",
+    // origin: "https://e-store-fe.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
